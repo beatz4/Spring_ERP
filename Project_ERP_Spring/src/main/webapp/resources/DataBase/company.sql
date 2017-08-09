a1977144bc18918681cd53aca4fe7d8e2e8d6b7a
jonghyeon Noh <user@user-pc>
2017-08-09 17:26:46 +0900

    company db id 추가

--- Project_ERP_Spring/src/main/webapp/resources/DataBase/company.sql
+++ Project_ERP_Spring/src/main/webapp/resources/DataBase/company.sql
@@ -11,8 +11,9 @@ ALTER SEQUENCE seq_company_idx INCREMENT BY 1
 -- 테이블 생성
 create table company (
 	idx				int,					-- 일련 번호
-	name			varchar2(100) unique,			-- 조직(팀) 이름
-	parent_idx		int not null,					-- 상부 조직의 인덱스 ( 4-1 이면 4번이 된다 ) tree구조를 위함
+	id				int unique,
+	name			varchar2(100),			-- 조직(팀) 이름
+	parent_idx		int,					-- 상부 조직의 인덱스 ( 4-1 이면 4번이 된다 ) tree구조를 위함
 	description		varchar2(500)			-- 설명
 );
 
@@ -26,57 +27,68 @@ alter table company add constraint pk_company_idx primary key(idx)
 
 -- 1
 insert into company values( seq_company_idx.nextVal,
+								1,
 								'대표',
 								0,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'대표자명단');
 
 -- 2
 insert into company values( seq_company_idx.nextVal,
+								2,
 								'영업부',
 								0,					-- 대표 index(1) + 부대표 index(2) : 12
 								'영업부서');
 								
 -- 3
 insert into company values( seq_company_idx.nextVal,
+								3,
 								'개발부',
 								0,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'개발부서');
 								
 -- 4
 insert into company values( seq_company_idx.nextVal,
+								4,
 								'경영지원부',
 								0,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'경영지원부서');
 
 -- 1:대표, 2:영업부, 3:개발부, 4:경영지원부
 insert into company values( seq_company_idx.nextVal,
+								101,
 								'대표이사',
 								1,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'CEO');
 								
 insert into company values( seq_company_idx.nextVal,
+								201,
 								'영업부서1',
 								2,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'영업1팀');
 insert into company values( seq_company_idx.nextVal,
+								202,
 								'영업부서2',
 								2,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'영업2팀');   
 								
 insert into company values( seq_company_idx.nextVal,
+								301,
 								'개발부1',
 								3,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'개발1팀');
 insert into company values( seq_company_idx.nextVal,
+								302,
 								'개발부2',
 								3,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'개발2팀');
 								
 insert into company values( seq_company_idx.nextVal,
+								401,
 								'경영지원부1',
 								4,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'경영1팀');
 insert into company values( seq_company_idx.nextVal,
+								402,
 								'경영지원부2',
 								4,					-- 대표자의 상부 조직은 없으므로 0을 준다.
 								'경영2팀');
