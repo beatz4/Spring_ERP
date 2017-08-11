0338b155929891a45ce3d875b9c550b9612bb808
jonghyeon Noh <user@user-pc>
2017-08-11 14:17:19 +0900

    companyvoarray commit

---
+++ Project_ERP_Spring/src/main/java/vo/CompanyVoArray.java
@@ -0,0 +1,15 @@
+package vo;
+
+import java.util.List;
+
+public class CompanyVoArray {
+	List<CompanyVo> list_company;
+
+	public List<CompanyVo> getList_company() {
+		return list_company;
+	}
+
+	public void setList_company(List<CompanyVo> list_company) {
+		this.list_company = list_company;
+	}
+}
