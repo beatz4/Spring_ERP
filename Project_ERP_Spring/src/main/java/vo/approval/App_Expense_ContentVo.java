79f571113163f881a1ef73ab8d3b2f6f485a4d7f
sam <kimjinsam@naver.com>
2017-08-17 14:37:20 +0900

    -Approval_Expense_ContentVo 생성

---
+++ Project_ERP_Spring/src/main/java/vo/approval/App_Expense_ContentVo.java
@@ -0,0 +1,81 @@
+package vo.approval;
+
+public class App_Expense_ContentVo {
+
+	int t_expense_idx;
+	int c_expense_idx, c_expense_money;
+	String c_expense_date, c_expense_content, c_expense_customer, c_expense_mepay, c_expense_et, c_expense_proof;
+
+	public int getT_expense_idx() {
+		return t_expense_idx;
+	}
+
+	public void setT_expense_idx(int t_expense_idx) {
+		this.t_expense_idx = t_expense_idx;
+	}
+
+	public int getC_expense_idx() {
+		return c_expense_idx;
+	}
+
+	public void setC_expense_idx(int c_expense_idx) {
+		this.c_expense_idx = c_expense_idx;
+	}
+
+	public int getC_expense_money() {
+		return c_expense_money;
+	}
+
+	public void setC_expense_money(int c_expense_money) {
+		this.c_expense_money = c_expense_money;
+	}
+
+	public String getC_expense_date() {
+		return c_expense_date;
+	}
+
+	public void setC_expense_date(String c_expense_date) {
+		this.c_expense_date = c_expense_date;
+	}
+
+	public String getC_expense_content() {
+		return c_expense_content;
+	}
+
+	public void setC_expense_content(String c_expense_content) {
+		this.c_expense_content = c_expense_content;
+	}
+
+	public String getC_expense_customer() {
+		return c_expense_customer;
+	}
+
+	public void setC_expense_customer(String c_expense_customer) {
+		this.c_expense_customer = c_expense_customer;
+	}
+
+	public String getC_expense_mepay() {
+		return c_expense_mepay;
+	}
+
+	public void setC_expense_mepay(String c_expense_mepay) {
+		this.c_expense_mepay = c_expense_mepay;
+	}
+
+	public String getC_expense_et() {
+		return c_expense_et;
+	}
+
+	public void setC_expense_et(String c_expense_et) {
+		this.c_expense_et = c_expense_et;
+	}
+
+	public String getC_expense_proof() {
+		return c_expense_proof;
+	}
+
+	public void setC_expense_proof(String c_expense_proof) {
+		this.c_expense_proof = c_expense_proof;
+	}
+
+}
